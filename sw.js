6965cd21e98d5e68a261a1b62fe395bdf34e724c
Nirakash <83993087+Niracash@users.noreply.github.com>
2026-05-15 01:28:01 +0200

    Upgrade service worker to use cache v2 and add icons

--- sw.js
+++ sw.js
@@ -1,20 +1,54 @@
-const CACHE = 'casino-v1';
-const ASSETS = ['/casino/', '/casino/index.html', '/casino/style.css', '/casino/app.js', '/casino/manifest.json'];
+const CACHE = "casino-v2";
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+const FILES = [
+  "/casino/",
+  "/casino/index.html",
+  "/casino/style.css",
+  "/casino/app.js",
+  "/casino/manifest.json",
+  "/casino/icon-192.png",
+  "/casino/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(FILES))
+      .catch(err => console.log(err))
+  );
+
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key=>{
+          if(key!==CACHE){
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
   self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const clone=response.clone();
+
+        caches.open(CACHE)
+          .then(cache=>cache.put(event.request,clone));
+
+        return response;
+      })
+      .catch(()=>{
+        return caches.match(event.request);
+      })
   );
 });
