865299040bb169a178aa766ceca72b67c02429e8
Nirakash <83993087+Niracash@users.noreply.github.com>
2026-05-15 14:00:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,57 @@
-const CACHE = "casino-v3";
+const CACHE = “casino-v3”;
 
 const FILES = [
-  "/casino/",
-  "/casino/index.html",
-  "/casino/style.css",
-  "/casino/app.js",
-  "/casino/manifest.json",
-  "/casino/icon-192.png",
-  "/casino/icon-512.png"
+“/casino/”,
+“/casino/index.html”,
+“/casino/style.css”,
+“/casino/app.js”,
+“/casino/manifest.json”,
+“/casino/icon-192.png”,
+“/casino/icon-512.png”
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
+self.addEventListener(“install”, event => {
+event.waitUntil(
+caches.open(CACHE)
+.then(cache => cache.addAll(FILES))
+.catch(err => console.log(err))
+);
+
+self.skipWaiting();
+});
+
+self.addEventListener(“activate”, event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(
+keys.map(key=>{
+if(key!==CACHE){
+return caches.delete(key);
+}
+})
+)
+)
+);
+
+self.clients.claim();
+});
+
+self.addEventListener(“fetch”, event => {
+event.respondWith(
+fetch(event.request)
+.then(response=>{
+const clone=response.clone();
+
+```
     caches.open(CACHE)
-      .then(cache => cache.addAll(FILES))
-      .catch(err => console.log(err))
-  );
-  self.skipWaiting();
-});
+      .then(cache=>cache.put(event.request,clone));
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key=>{
-          if(key!==CACHE){
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
+    return response;
+  })
+  .catch(()=>{
+    return caches.match(event.request);
+  })
+```
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response=>{
-        const clone=response.clone();
-        caches.open(CACHE)
-          .then(cache=>cache.put(event.request,clone));
-        return response;
-      })
-      .catch(()=>{
-        return caches.match(event.request);
-      })
-  );
+);
 });
